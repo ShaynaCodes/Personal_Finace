66a08bca6b51a65bfbdbf56387cc13fc682b906f
Shayna Fort-Foskey <shayna@Shaynas-Air.fios-router.home>
2024-08-31 11:40:00 -0400

    base of project

---
+++ script.js
@@ -0,0 +1,22 @@
+function budget(){
+    var income = document.getElementById("budget").value;
+    let save; let fun; let needs;
+    if(isNaN(income)){
+        window.alert("This is not a number. Try again")
+    }
+    save = income * .20
+    fun = income * .30
+    needs = income * .50 
+    console.log( save, fun, needs)
+}
+
+function confirm_password(){
+    var password = document.getElementById('password')
+    var c_password= document.getElementById('cpassword')
+
+    if(password.value != c_password.value){
+        c_password.setCustomValidity("Passwords Don't Match");
+    }else{
+        c_password.setCustomValidity('');
+    }
+}
