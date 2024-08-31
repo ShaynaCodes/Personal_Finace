47a672c8d3f98086ff22fa2b5e66825c1d5cedf9
Shayna <sf363@njit.edu>
2024-08-31 19:13:42 -0400

    fixed backend and can now add to database

--- server/server.js
+++ server/server.js
@@ -9,13 +9,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'users',
+    database: 'finance',
     password: '10coolgirl',
     port: 5432, 
   });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '', '../client/pages/signup.html'));
+});
+
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM users;';
+
+  pool.query(query, (error, result) => {
+    if (error) {
+      console.error('Error occurred:', error);
+      res.status(500).send('An error occurred while retrieving data from the database.');
+    } else {
+      const users = result.rows;
+      res.json(users);
+    }
+  });
+});
+
 app.post('/register', (req, res) => {
-    const { firstname, lastname, email, password } = req.body;
+    const {firstname, lastname, email, password } = req.body;
 
     pool.query('INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4)', [firstname, lastname, email, password], (error, results) => {
         if (error) {
@@ -28,7 +46,7 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
     
-    pool.query('SELECT * FROM users WHERE email = $3 AND password = $4', [email, password], (error, results) => {
+    pool.query('SELECT * FROM users WHERE email = $4 AND password = $5', [email, password], (error, results) => {
       if (error) {
         throw error;
       }
